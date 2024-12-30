b362ceda6d82a69ff3ce0cc211517fa40fd8a07b
vishwang suthar <100853098+Vishwang0Suthar@users.noreply.github.com>
2024-12-30 20:56:00 +0530

    added hover animation to contact section along with mobile view optimization

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,118 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./contact.css";
+import { contactData } from "@/lib/constData";
 import Logospring from "./logoSpring";
 type Props = {};
 
 const Contact = (props: Props) => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth > 1200);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth > 1200);
+    };
+
+    // Add event listener for resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="lg:my-0 my-6 overflow-hidden contact border-2 border-gray-400 h-fit flex gap-2 justify-between flex-col w-full ">
       <div className="lg:contact-div p-8 py-2 flex flex-col divide-y-2 divide-gray-400 gap-0">
-        <div className="h-fit w-full  lg:px-6 p-5 lg:p-7 justify-between items-center flex gap-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="60"
-            height="60"
-            viewBox="0 0 64 64"
-            className="scale-100 fill-current  text-white duration-500 hover:text-black"
-          >
-            <path d="M32 6C17.641 6 6 17.641 6 32c0 12.277 8.512 22.56 19.955 25.286-.592-.141-1.179-.299-1.755-.479V50.85c0 0-.975.325-2.275.325-3.637 0-5.148-3.245-5.525-4.875-.229-.993-.827-1.934-1.469-2.509-.767-.684-1.126-.686-1.131-.92-.01-.491.658-.471.975-.471 1.625 0 2.857 1.729 3.429 2.623 1.417 2.207 2.938 2.577 3.721 2.577.975 0 1.817-.146 2.397-.426.268-1.888 1.108-3.57 2.478-4.774-6.097-1.219-10.4-4.716-10.4-10.4 0-2.928 1.175-5.619 3.133-7.792C19.333 23.641 19 22.494 19 20.625c0-1.235.086-2.751.65-4.225 0 0 3.708.026 7.205 3.338C28.469 19.268 30.196 19 32 19s3.531.268 5.145.738c3.497-3.312 7.205-3.338 7.205-3.338.567 1.474.65 2.99.65 4.225 0 2.015-.268 3.19-.432 3.697C46.466 26.475 47.6 29.124 47.6 32c0 5.684-4.303 9.181-10.4 10.4 1.628 1.43 2.6 3.513 2.6 5.85v8.557c-.576.181-1.162.338-1.755.479C49.488 54.56 58 44.277 58 32 58 17.641 46.359 6 32 6zM33.813 57.93C33.214 57.972 32.61 58 32 58 32.61 58 33.213 57.971 33.813 57.93zM37.786 57.346c-1.164.265-2.357.451-3.575.554C35.429 57.797 36.622 57.61 37.786 57.346zM32 58c-.61 0-1.214-.028-1.813-.07C30.787 57.971 31.39 58 32 58zM29.788 57.9c-1.217-.103-2.411-.289-3.574-.554C27.378 57.61 28.571 57.797 29.788 57.9z"></path>
-          </svg>
-          <div className=" contact-text h-8 overflow-hidden">
-            <p className="lnkx  text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
-              <a target="_blank" href="https://github.com/Vishwang0Suthar">
-                /vishwang0suthar
-              </a>
-            </p>
-            <p className="lnkx-2 text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
-              <a target="_blank" href="https://github.com/Vishwang0Suthar">
-                /vishwang0suthar
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="h-fit  lg:px-6 p-5 lg:p-7 fill-current transition-all duration-500 w-full justify-between items-center flex gap-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="50"
-            height="50"
-            viewBox="0 0 32 32"
-            className="scale-125  text-white duration-500 hover:text-black"
-          >
-            <path d="M 11.46875 5 C 7.917969 5 5 7.914063 5 11.46875 L 5 20.53125 C 5 24.082031 7.914063 27 11.46875 27 L 20.53125 27 C 24.082031 27 27 24.085938 27 20.53125 L 27 11.46875 C 27 7.917969 24.085938 5 20.53125 5 Z M 11.46875 7 L 20.53125 7 C 23.003906 7 25 8.996094 25 11.46875 L 25 20.53125 C 25 23.003906 23.003906 25 20.53125 25 L 11.46875 25 C 8.996094 25 7 23.003906 7 20.53125 L 7 11.46875 C 7 8.996094 8.996094 7 11.46875 7 Z M 21.90625 9.1875 C 21.402344 9.1875 21 9.589844 21 10.09375 C 21 10.597656 21.402344 11 21.90625 11 C 22.410156 11 22.8125 10.597656 22.8125 10.09375 C 22.8125 9.589844 22.410156 9.1875 21.90625 9.1875 Z M 16 10 C 12.699219 10 10 12.699219 10 16 C 10 19.300781 12.699219 22 16 22 C 19.300781 22 22 19.300781 22 16 C 22 12.699219 19.300781 10 16 10 Z M 16 12 C 18.222656 12 20 13.777344 20 16 C 20 18.222656 18.222656 20 16 20 C 13.777344 20 12 18.222656 12 16 C 12 13.777344 13.777344 12 16 12 Z"></path>
-          </svg>
-
-          <p className="lnkx text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
-            <a
-              target="_blank"
-              href="https://www.instagram.com/vishwang._.suthar/"
-            >
-              /vishwang._.suthar
-            </a>
-          </p>
-        </div>
-        <div className="h-fit w-full lg:px-6 p-5 lg:p-7 justify-between items-center flex gap-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="50"
-            height="50"
-            viewBox="0 0 50 50"
-            className="scale-100 fill-current  text-white duration-500 hover:text-black"
-          >
-            <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
-          </svg>
-
-          <p className="lnkx text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
-            <a
-              target="_blank"
-              href="https://www.linkedin.com/in/vishwang-suthar"
-            >
-              /vishwang-suthar
-            </a>
-          </p>
-        </div>
-        <div className="h-fit w-full lg:px-6 p-5 lg:p-7 justify-between items-center flex gap-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="50"
-            height="50"
-            viewBox="0 0 50 50"
-            className="scale-125 fill-current text-white duration-500 hover:text-black"
-          >
-            <path d="M25.009,1.982C12.322,1.982,2,12.304,2,24.991S12.322,48,25.009,48s23.009-10.321,23.009-23.009S37.696,1.982,25.009,1.982z M34.748,35.333c-0.289,0.434-0.765,0.668-1.25,0.668c-0.286,0-0.575-0.081-0.831-0.252C30.194,34.1,26,33,22.5,33.001 c-3.714,0.002-6.498,0.914-6.526,0.923c-0.784,0.266-1.635-0.162-1.897-0.948s0.163-1.636,0.949-1.897 c0.132-0.044,3.279-1.075,7.474-1.077C26,30,30.868,30.944,34.332,33.253C35.022,33.713,35.208,34.644,34.748,35.333z M37.74,29.193 c-0.325,0.522-0.886,0.809-1.459,0.809c-0.31,0-0.624-0.083-0.906-0.26c-4.484-2.794-9.092-3.385-13.062-3.35 c-4.482,0.04-8.066,0.895-8.127,0.913c-0.907,0.258-1.861-0.272-2.12-1.183c-0.259-0.913,0.272-1.862,1.184-2.12 c0.277-0.079,3.854-0.959,8.751-1c4.465-0.037,10.029,0.61,15.191,3.826C37.995,27.328,38.242,28.388,37.74,29.193z M40.725,22.013 C40.352,22.647,39.684,23,38.998,23c-0.344,0-0.692-0.089-1.011-0.275c-5.226-3.068-11.58-3.719-15.99-3.725 c-0.021,0-0.042,0-0.063,0c-5.333,0-9.44,0.938-9.481,0.948c-1.078,0.247-2.151-0.419-2.401-1.495 c-0.25-1.075,0.417-2.149,1.492-2.4C11.729,16.01,16.117,15,21.934,15c0.023,0,0.046,0,0.069,0 c4.905,0.007,12.011,0.753,18.01,4.275C40.965,19.835,41.284,21.061,40.725,22.013z"></path>
-          </svg>
-
-          <p className="lnkx text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
-            <a
-              target="_blank"
-              href="https://open.spotify.com/user/l2xp29n2wnq3wbz59yvj6bc6c?si=JuQeuJdeSL-NXyNgBIZxFQ&utm_source=copy-link"
-            >
-              /drip-monke
-            </a>
-          </p>
-        </div>
-        <div className="h-fit w-full lg:px-6 p-5 lg:p-7 justify-between items-center flex gap-6">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="48"
-            height="48"
-            viewBox="0 0 50 50"
-            className=" fill-white scale-125  text-white duration-500 hover:fill-black"
-          >
-            <path d="M 11 4 C 7.1456661 4 4 7.1456661 4 11 L 4 39 C 4 42.854334 7.1456661 46 11 46 L 39 46 C 42.854334 46 46 42.854334 46 39 L 46 11 C 46 7.1456661 42.854334 4 39 4 L 11 4 z M 11 6 L 39 6 C 41.773666 6 44 8.2263339 44 11 L 44 39 C 44 41.773666 41.773666 44 39 44 L 11 44 C 8.2263339 44 6 41.773666 6 39 L 6 11 C 6 8.2263339 8.2263339 6 11 6 z M 13.085938 13 L 22.308594 26.103516 L 13 37 L 15.5 37 L 23.4375 27.707031 L 29.976562 37 L 37.914062 37 L 27.789062 22.613281 L 36 13 L 33.5 13 L 26.660156 21.009766 L 21.023438 13 L 13.085938 13 z M 16.914062 15 L 19.978516 15 L 34.085938 35 L 31.021484 35 L 16.914062 15 z"></path>
-          </svg>
-
-          <p className="lnkx text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
-            <a target="_blank" href="https://twitter.com/motox3n_sempai">
-              /motox3n_sempai
-            </a>
-          </p>
-        </div>
+        {contactData.map((contact, index) => {
+          const Icon = contact.icon;
+          return (
+            <>
+              <div className="h-fit w-full  lg:px-6 p-5 lg:p-7 justify-between items-center flex gap-6">
+                <Icon className="scale-100 fill-current  text-white duration-500 hover:text-black" />
+                <div
+                  className={`h-8 overflow-hidden ${
+                    isLargeScreen ? "contact-text" : ""
+                  }`}
+                >
+                  <p
+                    className={`lnkx ${
+                      isLargeScreen ? "" : "hover:text-lightBlue-100"
+                    }  text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end`}
+                  >
+                    <a target="_blank" href={contact.url}>
+                      {contact.name}
+                    </a>
+                  </p>
+                  {isLargeScreen ? (
+                    <p className="lnkx-2  duration-300 text-sm lg:text-lg line-clamp-1 min-w-32 lg:min-w-44  text-end">
+                      <a target="_blank" href={contact.url}>
+                        {contact.name}
+                      </a>
+                    </p>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+              </div>
+            </>
+          );
+        })}
       </div>
       <Logospring />
     </div>
---
+++ src/lib/constData.ts
@@ -0,0 +1,42 @@
+import Spotify from "@/public/icons/spotify.svg"
+import Twitter from "@/public/icons/twitter.svg"
+import Instagram from "@/public/icons/instagram.svg"
+import Linkedin from "@/public/icons/linkedin.svg"
+import Github from "@/public/icons/github.svg"
+
+
+export const contactData = [
+        {
+            id: 1,
+            name: "/vishwang0suthar",
+            icon: Github,
+            url: "https://github.com/Vishwang0Suthar",
+        },
+        {
+          id: 2,
+          name: "/vishwang._.suthar",
+          icon: Instagram,
+          url: "https://www.instagram.com/vishwang._.suthar/",
+        },
+        {
+          id: 3,
+          name: "/vishwang-suthar",
+          icon: Linkedin,
+          url: "https://www.linkedin.com/in/vishwang-suthar",
+        },
+        {
+          id: 4,
+          name: "/drip-monke",
+          icon: Spotify,
+          url: "https://open.spotify.com/user/l2xp29n2wnq3wbz59yvj6bc6c?si=JuQeuJdeSL-NXyNgBIZxFQ&utm_source=copy-link",
+          
+        },
+        {
+          id: 5,
+          name: "/motox3n_sempai",
+          icon: Twitter,
+          url: "https://twitter.com/motox3n_sempai",
+        },
+    
+
+]
